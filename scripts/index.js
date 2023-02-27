7c6bc58905cbb918d8fca2e58be6dfcaf2b1e1f1
Luciano Vidal <70247350+Lucho-Vidal@users.noreply.github.com>
2023-02-27 13:52:38 -0300

    Se cambiaron las tarjetas estaticas por dinamicas y filtradas por fechas en upcomingEvents y pastEvent.

--- scripts/index.js
+++ scripts/index.js
@@ -1,23 +1,29 @@
-const contTarjeta = document.querySelector("#containerCard");
 
-let tarjetasGeneradas = crearTarjetas(data.events)
-
-contTarjeta.innerHTML = tarjetasGeneradas;
-
-
-function crearTarjetas(arrayEvents) {
+function generarTarjetas(arrayEvents,currentDate){
     let tarjetas = "";
     for (const event of arrayEvents) {
-        tarjetas += `
-        <div class="card" style="width: 18rem;">
-            <img src="${event.image}" class="card-img-top" alt="Images">
-            <div class="card-body">
-                <h5 class="card-title">${event.name}</h5>
-                <p class="card-text">${event.description}</p>
-                <a href="#" class="btn btn-primary">See more</a>
-            </div>
-        </div>`
+        tarjetas += crearTarjeta(event);
     }
     return tarjetas
 }
 
+function crearTarjeta(event) {
+        return `
+        <div class="card" style="width: 18rem;">
+            <img src="${event.image}" class="card-img-top" alt="Images">
+            <div class="card-body">
+                <h5 class="card-title">${event.name}</h5>
+                <P>Date: ${event.date}</p>
+                <p class="card-text">${event.description}</p>
+                <p>Category: ${event.category}</p>
+                <p>Price: ${event.price}</P>
+                <a href="#" class="btn btn-primary">See more</a>
+            </div>
+        </div>`
+}
+
+const contTarjeta = document.querySelector("#containerCard");
+
+let tarjetasGeneradas = generarTarjetas(data.events,data.currentDate)
+
+contTarjeta.innerHTML = tarjetasGeneradas;
---
+++ scripts/upcomingEvents.js
@@ -0,0 +1,31 @@
+
+function generarTarjetas(arrayEvents,currentDate){
+    let tarjetas = "";
+    for (const event of arrayEvents) {
+        if(Date.parse(event.date)>Date.parse(currentDate)){
+            tarjetas += crearTarjeta(event);
+        }
+    }
+    return tarjetas
+}
+
+function crearTarjeta(event) {
+        return `
+        <div class="card" style="width: 18rem;">
+            <img src="${event.image}" class="card-img-top" alt="Images">
+            <div class="card-body">
+                <h5 class="card-title">${event.name}</h5>
+                <P>Date: ${event.date}</p>
+                <p class="card-text">${event.description}</p>
+                <p>Category: ${event.category}</p>
+                <p>Price: ${event.price}</P>
+                <a href="#" class="btn btn-primary">See more</a>
+            </div>
+        </div>`
+}
+
+const contTarjeta = document.querySelector("#containerCard");
+
+let tarjetasGeneradas = generarTarjetas(data.events,data.currentDate)
+
+contTarjeta.innerHTML = tarjetasGeneradas;
