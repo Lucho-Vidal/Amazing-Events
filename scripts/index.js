1508d33a10f08654a949bc608e1dfdf85f95a557
Lucho-Vidal <luccho277@gmail.com>
2023-03-18 19:16:39 -0300

    Se pasaron las funciones a funciones.js

--- scripts/index.js
+++ scripts/index.js
@@ -7,78 +7,6 @@ function generarTarjetas(arrayEvents){
     return tarjetas
 }
 
-function crearTarjeta(event) {
-    return `
-        <div id="event" class="card d-flex justify-content-center" style="width: 18rem;">
-            <img src="${event.image}" class="card-img-top" alt="Images">
-            <div class="card-body">
-                <h5 class="card-title">${event.name}</h5>
-                <P>Date: ${event.date}</p>
-                <div class="card-bottom d-flex flex-column justify-content-between">
-                    <p class="card-text">${event.description}</p>
-                    <p>Price: $ ${event.price}</P>
-                    <input type="button"  onclick="seeDetail('${event._id}')" value="See more" class="btn btn-primary">
-                </div>
-                
-            </div>
-        </div>`
-}
-
-function seeDetail(id) {
-    window.location.href = `./event.html?id=${id}`
-}
-//Categorias
-function cargarCategorias(arrayCat){
-    let categorias = "";
-    for (let i=0;i<arrayCat.length;i++){
-        categorias +=  crearCheckbox(arrayCat[i],i);
-    }
-    return categorias
-}
-
-function crearCheckbox(cat,i){
-    return `
-    <div class="form-check form-check-inline m-0">
-        <input class="form-check-input valoresCheck" type="checkbox" name="categorias" id="categoria${i}" value="${cat}"   />
-        <label class="form-check-label" for="categoria${i}">${cat}</label>
-    </div>`
-}
-function filtrarCheckbox(events,checkbox){
-    let eventfiltrados = [];
-    if(checkbox.length > 0){
-        checkbox.forEach((categoria)=>{
-            events.forEach((event)=>{
-                if(event.category==categoria){
-                    eventfiltrados.push(event);
-                }
-            });        
-        });
-    }else{
-        eventfiltrados = events;
-    }
-    return eventfiltrados;
-}
-
-function buscar(){
-    let eventosEncontrados = [];
-    let eventCheckbox = filtrarCheckbox(data.events,categoriaSelect);
-    eventosEncontrados = eventCheckbox.filter((event)=>{
-        return eventosFiltrados = (event.name.toLowerCase().includes(buscador.value.toLowerCase()));
-    });
-    return eventosEncontrados;
-}
-
-//quiero sacar los duplicados del array
-function eliminarDuplicados (array){
-    let unicos = []
-    for(let i = 0 ; i< array.length; i++){
-        if (!unicos.includes(array[i])){
-            unicos.push(array[i])
-        }
-    }
-    return unicos
-}
-
 let categoriaSelect = []
 
 //tarjetas
@@ -116,5 +44,3 @@ buscador.addEventListener('keyup',()=> {
     let eventosEncontrados = buscar();
     contTarjeta.innerHTML = generarTarjetas(eventosEncontrados);
 });
-
-let 
